d118dd5e8fe2ebf5fa321487684543f1dd6ac324
Zach <zvollink@gmail.com>
2018-07-25 06:17:33 +0000

    Additional fix attempt

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SimplecodecastsSaas
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
     
     # config/application.rb
     config.assets.initialize_on_precompile = false
